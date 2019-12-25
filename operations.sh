b09fb7f0fb1b931529908feaf18789814e6c9f86
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2019-12-25 21:29:43 +0100

    Update operations.sh

--- operations.sh
+++ operations.sh
@@ -7,9 +7,9 @@
 docker-compose up -d 
 
 docker exec -it apm_build_from_src sh -c "/pipeline/ops/bin/apm --version"
-apm  2.4.3
-npm  6.2.0
-node 8.16.2 x64
-atom unknown
-python 2.7.16
-git 2.22.2
+# apm  2.4.3
+# npm  6.2.0
+# node 8.16.2 x64
+# atom unknown
+# python 2.7.16
+# git 2.22.2
